e6917b21c949a3397421c620a80b508a3dd1f1e8
Smita Panda <smitaranipanda@gmail.com>
2017-04-13 09:57:49 +0100

    Committed JPMC Java Technical Test-Daily Report

---
+++ daily-report/src/main/java/com/jpmorgan/test/HelperUtil.java
@@ -0,0 +1,174 @@
+package com.jpmorgan.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+public class HelperUtil 
+{
+	public enum SaleType 
+	{
+		/* Buy */
+		B,
+		/* Sell */
+		S
+	}
+	
+	/**
+	 * 
+	 * @param items
+	 */
+	public static void printEntityList(ArrayList<Item> items)
+	{
+		System.out.println("==================================================Entity List ================================================================");
+		System.out.println("Entity, Buy/Sell, AgreedFx, Currency, Instruction Date , Settlement Date, Actual Settlement Date, Units, Price per Unit, Rank");
+		System.out.println("==============================================================================================================================");
+		for (int i =0; i <items.size(); i ++)
+		{
+			Item itm = items.get(i);
+			System.out.println(itm.toString());
+		}
+	}
+	
+	/**
+	 * Print Daily Report for incoming and outgoing transactions
+	 * @param items
+	 */
+	public static void printDailySummary(ArrayList<Item> items) 
+	{
+		HashMap<Date,DailyReport> reportSummary = new HashMap<Date,DailyReport>();
+		for (int i =0; i <items.size(); i ++)
+		{
+			Item itm = items.get(i);
+			DailyReport report = reportSummary.get(itm.getActualSettlementDate());
+			if (report==null)
+			{
+				report = new DailyReport(0.0f, 0.0f);
+			}
+			report.incrementSales(itm);
+			reportSummary.put(itm.getActualSettlementDate(), report);
+		}
+		
+		System.out.println("=============Daily Report ========================");
+		Iterator<Entry<Date, DailyReport>> itr = reportSummary.entrySet().iterator();
+		while(itr.hasNext())
+		{
+			Entry<Date, DailyReport> value =itr.next();
+			System.out.println("Date:"+value.getKey()+"  Incoming transaction:"+ value.getValue().getIncomingTransaction()+" USD"
+			+" Outgoing transaction:"+ value.getValue().getOutgoingtTransaction()+" USD");
+		}
+		System.out.println("===========End of Report=======================");
+	}
+	
+	/**
+	 * Sort by total price and then assign Rank
+	 * @param items
+	 */
+	public static void sortAndRankCommodity(ArrayList<Item> items)
+	{
+		Collections.sort(items, new Comparator<Item>() 
+		{
+			@Override
+		    public int compare(Item itm1, Item itm2) 
+		    {				
+				if (itm1.getAgreedFx()*itm1.getUnitCount()*itm1.getUnitPrice() 
+						== itm2.getAgreedFx()*itm2.getUnitCount()*itm2.getUnitPrice())
+				{
+					return 0;
+				}
+				else if (itm1.getAgreedFx()*itm1.getUnitCount()*itm1.getUnitPrice() 
+						> itm2.getAgreedFx()*itm2.getUnitCount()*itm2.getUnitPrice())
+				{
+					return -1;
+				}
+				else
+				{
+					return 1;
+				}
+				
+			}
+		});
+		
+		int currentOutgoingRank = 1;
+		int currentIncomingRank = 1;
+		for(int i =0; i <items.size();i++)
+		{
+			if (items.get(i).getSaleType() == SaleType.B)
+			{
+				items.get(i).setRank(currentOutgoingRank);
+				currentOutgoingRank++;
+			}
+			else
+			{
+				items.get(i).setRank(currentIncomingRank);
+				currentIncomingRank++;
+			}
+		}
+		printEntityList(items);
+	}
+
+	/**
+	 * Reset Rank for all items
+	 * @param items
+	 */
+	public static void resetCommodityRanking(ArrayList<Item> items) 
+	{
+		for(int i =0; i <items.size();i++)
+		{
+			items.get(i).setRank(-1);
+		}
+	}
+		
+	/**
+	 * This method Ranks all the Commodity without Sorting
+	 * @param items
+	 */
+	public static void rankCommodity(ArrayList<Item> items) 
+	{
+		int maxOutgoingRank =0;
+		int maxIncomingRank =0;
+		for(int i =0; i <items.size();i++)
+		{
+			if (items.get(i).getSaleType() == SaleType.B)
+			{
+				maxOutgoingRank++;
+			}
+			else
+			{
+				maxIncomingRank++;
+			}
+			
+		}
+		
+		for (int i =0; i <items.size();i++)
+		{
+			int count=0;
+			for (int j=0; j<items.size();j++)
+			{
+				if (items.get(i).getSaleType() == items.get(j).getSaleType() && i!=j)
+				{
+					float outerItemTotal = items.get(i).getAgreedFx() * items.get(i).getUnitCount()* items.get(i).getUnitPrice();
+					float innerItemTotal = items.get(j).getAgreedFx() * items.get(j).getUnitCount()* items.get(j).getUnitPrice();
+					if (innerItemTotal < outerItemTotal 
+							|| (innerItemTotal == outerItemTotal && items.get(j).getRank() !=-1))
+					{
+						count++;
+					}			
+				}
+			}
+			if (items.get(i).getSaleType() == SaleType.B)
+			{
+				items.get(i).setRank(maxOutgoingRank-count);
+			}
+			else
+			{
+				items.get(i).setRank(maxIncomingRank-count);
+			}
+		}
+		printEntityList(items);
+	}
+}
---
+++ daily-report/src/main/java/com/jpmorgan/test/MarketMain.java
@@ -0,0 +1,30 @@
+package com.jpmorgan.test;
+
+import java.util.ArrayList;
+
+import com.jpmorgan.test.HelperUtil.SaleType;
+
+public class MarketMain {
+
+	public static void main(String[] args) 
+	{
+		ArrayList<Item> items = new ArrayList<Item>();
+		
+		items.add(new Item("foo", SaleType.B, 0.50f, "USD", "01/01/2016", "01/01/2016", 200, 100.25f));		
+		items.add(new Item("bar", SaleType.S, 0.22f, "AED", "05/01/2016", "07/01/2016", 450, 150.5f));		 
+		items.add(new Item("test1", SaleType.B, 0.22f, "SAR", "03/01/2016", "07/01/2017", 200, 150.5f));
+		items.add(new Item("test2", SaleType.B, 0.22f, "USD", "03/01/2016", "07/01/2017", 200, 150.5f));
+		items.add(new Item("test3", SaleType.B, 0.22f, "USD", "02/01/2016", "07/01/2017", 200, 150.5f));
+		
+		items.add(new Item("test4", SaleType.B, 0.22f, "USD", "04/01/2016", "04/01/2017", 300, 150.5f));
+		items.add(new Item("test4", SaleType.S, 0.22f, "USD", "04/01/2016", "04/01/2017", 33, 150.5f));
+		
+		HelperUtil.printDailySummary(items);
+		HelperUtil.rankCommodity(items);
+		
+		HelperUtil.resetCommodityRanking(items);
+		HelperUtil.sortAndRankCommodity(items);
+		
+	}
+
+}
